a73e0b9c9e46d3661b85f81cf277233bf4aba9ab
Max Kless <maxk@nrwl.io>
2024-04-19 11:10:18 +0200

    18.3.2

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
-
 export default defineConfig({
   root: __dirname,
   cacheDir: './node_modules/.vite/.',
